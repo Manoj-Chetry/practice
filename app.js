643667dce72d5a9985d3ba1b767c8d1393cdda19
Manoj Chetry <mchetry606@gmail.com>
2024-01-12 19:40:17 +0530

    completed

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let session  = require("express-session");
 
 var app = express();
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: "xyz"
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +28,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
+const userModel = require("./users");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  req.session.bane = true;
+  res.cookie("age", 25)
   res.render('index', { title: 'Express' });
 });
+router.get('/unban', function(req, res, next) {
+  req.session.destroy(function(err){
+    if(err) throw err;
+    res.send("Unbanned check");
+  })
+});
+
+router.get('/readcookie', function(req, res){
+  console.log(req.cookies);
+  res.send("Cookie")
+});
+router.get('/dcookie', function(req, res){
+  res.clearCookie("age");
+  res.send("Cookie deleted")
+});
+
+router.get("/check", function(req,res){
+  console.log(req.session.bane);
+  if(req.session.bane===true){
+    res.send("banned");
+  }else{
+    res.send("not banned");
+  }
+});
+
+router.get('/create', async function(req, res, next) {
+  const created = await userModel.create({
+    username: "Manoj",
+    name: "Manoj Chetry",
+    age: 25
+  });
+  res.send(created);
+});
+
+router.get('/allusers', async function(req, res){
+  let showusers = await userModel.find();
+  res.send(showusers);
+});
+router.get('/oneusers', async function(req, res){
+  let xyz = await userModel.findOne({username: "Manoj"});
+  res.send(xyz);
+});
+
+
+router.get("/delete", async function(req,res){
+  const del = await userModel.findOneAndDelete({
+    name: "Manoj Chetry"
+  })
+  res.send(del);
+})
+
 
 module.exports = router;
